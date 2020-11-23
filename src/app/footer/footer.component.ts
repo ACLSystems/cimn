65f89bbfc95adccc79149bd860e0e2fa68e025f8
Arturo Castro Loera <arturocastro@aclsystems.mx>
2020-11-23 16:59:47 -0600

    Modificación plantilla

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+	year: number;
+
+  constructor() {
+		const now = new Date();
+		this.year = now.getFullYear();
+	}
 
   ngOnInit(): void {
   }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 declare const $:any;
 
@@ -9,7 +10,9 @@ declare const $:any;
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+		private router: Router
+	) { }
 
   ngOnInit(): void {
 		const $dropdown = $(".dropdown");
