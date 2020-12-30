628dc198e4da7bfb977f72068a5d13beef5a52eb
Arturo Rodolfo Castro Loera <arturocastro@aclsystems.mx>
2020-12-30 11:42:25 -0600

    Servicio de soporte

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -12,6 +12,7 @@ import {
 } from './pipes';
 import {
 	UserService,
+	SupportingServices,
 	ValtransService
 } from './services';
 
@@ -32,6 +33,7 @@ export function tokenGetter() {
   ],
 	providers: [
 		UserService,
+		SupportingServices,
 		ValtransService
 	],
 	exports: [
